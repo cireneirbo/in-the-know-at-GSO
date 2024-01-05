9f738d65b8969138cd43d1f256075d00ed9992ec
Eric O'Brien <cireneirbo@gmail.com>
2024-01-04 20:14:11 -0500

    Add frontend populations

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,5 +8,10 @@ router.get('/', index_controller.index);
 /* GET local activities page. */
 router.get('/local-activities', index_controller.local_activities);
 
+/* GET local activities page. */
+router.get('/events', function(req, res, next) {
+  res.render('index', { title: 'In The Know: In GSO', data: index_controller.local_activities })
+});
+
 
 module.exports = router;
